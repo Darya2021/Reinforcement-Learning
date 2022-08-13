279fe169dd9c598f2605c94f2c970fa25b59efe6
Darya2021 <84893917+Darya2021@users.noreply.github.com>
2022-08-13 15:00:36 +0430

    Update Cart Pole.py

--- Cart Pole.py
+++ Cart Pole.py
@@ -4,23 +4,19 @@ import gym
 env = gym.make("CartPole-v0")
 print(env.action_space)
 print(env.observation_space)
-
-num_episodes = 100
+num_episod = 100
 num_timesteps = 50
-
-
-for i in range(num_episodes):
-
+for i in range(num_episod):
     Return = 0
     state = env.reset()
-    
-    
     for t in range(num_timesteps):
+        env.render()
         random_action = env.action_space.sample()
-        next_state, reward, done, info = env.step(random_action)
-        Return = Return + reward
-        
+        nexts, reward, done, info = env.step(random_action)
+        Return += reward
         if done:
             break
-            
-    print(f"random action {random_action} Episod {i}, Return {Return}\n")
+        if i%10 == 0:
+            print(f"random action {random_action} Episod {i}, Return {Return}\n")
+    
+    
